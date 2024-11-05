83d375e487d8bade701dc7d602d18a8a40116cdc
mohmed186 <97255835+mohmed186@users.noreply.github.com>
2024-11-06 00:17:20 +0200

    Add files via upload

---
+++ BusinessStarterSection/scripts.js
@@ -0,0 +1,7 @@
+// JavaScript for Toggle Menu
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.getElementById('nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
